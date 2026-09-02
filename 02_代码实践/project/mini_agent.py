3643155907c55e454bc50ad861d12371e70c1b48
smalMUMU <3276247891@qq.com>
2026-09-02 22:00:50 +0800

    2

--- 02_代码实践/project/mini_agent.py
+++ 02_代码实践/project/mini_agent.py
@@ -30,3 +30,33 @@ if __name__ == "__main__":
     print(run(input("你：")))
 
 #第二遍
+from pi_ai import call_llm
+from tools import TOOLS, run_tool
+
+SYSTEM = "你是一名代码助手" + str([t["function"]["name"] for t in TOOLS])
+
+def run(user_input):
+    messages = [
+        {"role": "system","content": SYSTEM},
+        {"role": "user","content": user_input},
+    ]
+    while True:
+        msg = call_llm(messages, TOOLS)
+        messages.append(msg)
+        tool_calls = msg.get("tool_calls")
+        if tool_calls:
+            for tc in tool_calls:
+                name = tc["function"]["name"]
+                args_json = tc["function"]["arguments"]
+                out = run_tool(name, args_json)
+                messages.append({
+                    "role": "tool",
+                    "tool_call_id": tc["id"],
+                    "content": out
+                }
+                )
+            continue
+        return msg.get("content")
+
+if __name__ == "__main__":
+    print(run(input("你:")))
