71a4d334daf79b85a56701b3294051e4ca294b75
smalMUMU <3276247891@qq.com>
2026-09-05 21:58:06 +0800

    5

--- 02_代码实践/project/mini_agent.py
+++ 02_代码实践/project/mini_agent.py
@@ -107,10 +107,11 @@ def run(user_input):
         {"role": "user","content": user_input},
     ]
     while True:
-        msg = call_llm(TOOLS, messages)
-        tool_calls = msg.get("tools")
+        msg = call_llm(messages, TOOLS)
+        messages.append(msg)
+        tool_calls = msg.get("tool_calls")
         if tool_calls:
-            for tc in TOOLS:
+            for tc in tool_calls:
                 name = tc["function"]["name"]
                 args_json = tc["function"]["arguments"]
                 out = run_tool(name, args_json)
@@ -124,4 +125,34 @@ def run(user_input):
 
 if __name__ == "__main__":
     print(run(input("你：")))
-    
+
+#第五遍
+from pi_ai import call_llm
+from tools import TOOLS, run_tool
+
+SYSTEM = "你是一名代码学习助手，辅助用户完成代码" + str([t["function"]["name"] for t in TOOLS])
+
+def run(user_input):
+    messages = [
+        {"role": "system","content": SYSTEM},
+        {"role": "user","content": user_input},
+    ]
+    while True:
+        msg = call_llm(messages, TOOLS)
+        messages.append(msg)
+        tool_calls = msg.get("tool_calls")
+        if tool_calls:
+            for tc in tool_calls:
+                name = tc["function"]["name"]
+                args_json = tc["function"]["arguments"]
+                out = run_tool(name, args_json)
+                messages.append({
+                    "role": "tool",
+                    "tool_call_id": tc["id"],
+                    "content": out
+                })
+            continue
+        return msg.get("content")
+
+if __name__ == "__main__":
+    print(run(input("你:")))
