a2fb7bcb5004c27e4fac6ebf0ad2a2edb61ea538
smalMUMU <3276247891@qq.com>
2026-09-06 17:05:37 +0800

    6

--- 02_代码实践/project/mini_agent.py
+++ 02_代码实践/project/mini_agent.py
@@ -156,3 +156,37 @@ def run(user_input):
 
 if __name__ == "__main__":
     print(run(input("你:")))
+
+#第六遍，此版本为M2版本，特点有长期记忆，和多轮对话记忆
+from pi_ai import call_llm
+from tools import TOOLS, run_tool
+
+SYSTEM = "你是一名编程学习助手，辅助用户完成编程学习" + str([t["function"]["name"] for t in TOOLS])
+
+messages = [{"role": "system","content": SYSTEM}]
+
+def run(user_input):
+    messages.append({"role": "user","content": user_input})
+    while True:
+        msg = call_llm(messages, TOOLS)
+        messages.append(msg)
+        tool_calls = msg.get("tool_calls")
+        if tool_calls:
+            for tc in tool_calls:
+                name = tc["function"]["name"]
+                args_json = tc["function"]["arguments"]
+                out = run_tool(name, args_json)
+                messages.append({
+                    "role": "tool",
+                    "tool_call_id": tc["id"],
+                    "content": out
+                })
+            continue
+        return msg.get("content")
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("你：")
+        if user_input == "/bye":
+            break
+        print(run(user_input))
