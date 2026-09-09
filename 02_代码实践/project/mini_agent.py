a6ab90dfca5937352cedb2ffd68b60726006a3a9
smalMUMU <3276247891@qq.com>
2026-09-09 23:30:00 +0800

    9.9

--- 02_代码实践/project/mini_agent.py
+++ 02_代码实践/project/mini_agent.py
@@ -231,7 +231,7 @@ from tools import TOOLS, run_tool
 
 SYSTEM = "你是一名编程学习助手，辅助用户进行代码学习" + str([t["function"]["name"] for t in TOOLS])
 
-messagges = [{"role": "system","content": SYSTEM}]
+messages = [{"role": "system","content": SYSTEM}]
 
 def run(user_input):
     messages.append({"role": "user","content": user_input})
@@ -240,7 +240,7 @@ def run(user_input):
         messages.append(msg)
         tool_calls = msg.get("tool_calls")
         if tool_calls:
-            for tc in TOOLS:
+            for tc in tool_calls:
                 name = tc["function"]["name"]
                 args_json = tc["function"]["arguments"]
                 out = run_tool(name, args_json)
@@ -251,6 +251,40 @@ def run(user_input):
                 })
             continue
         return msg.get("content")
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("你:")
+        if user_input == "/bye":
+            break
+        print(run(user_input))
+
+#第九遍M3，tool变化的版本
+from pi_ai import call_llm
+from tools import TOOLS, run_tool
+
+SYSTEM = "你是一名编程学习助手，主要作用是辅助用户代码学习" + str([t["function"]["name"] for t in TOOLS])
+
+messages = [{"role": "system","content": SYSTEM}]
+
+def run(user_input):
+    messages.append({"role": "user","content": user_input})
+    while True:
+        msg = call_llm(messages, TOOLS)
+        messages.append(msg)
+        tool_calls = msg.get("tool_calls")
+        if tool_calls:
+            for tc in tool_calls:
+                name = tc["function"]["name"]
+                args_json = tc["function"]["arguments"]
+                out = run_tool(name, args_json)
+                messages.append({
+                    "role": "tool",
+                    "tool_call_id": tc["id"],
+                    "content": out
+                })
+            continue
+        return msg,get("content")
 
 if __name__ == "__main__":
     while True:
