a64e107757b8b4b11e88a69b2eafbe02606a7a6f
smalMUMU <3276247891@qq.com>
2026-09-12 19:06:04 +0800

    pi_ai

--- 02_代码实践/project/mini_agent.py
+++ 02_代码实践/project/mini_agent.py
@@ -284,11 +284,46 @@ def run(user_input):
                     "content": out
                 })
             continue
-        return msg,get("content")
+        return msg.get("content")
 
 if __name__ == "__main__":
     while True:
         user_input = input("你:")
+        if user_input == "/bye":
+            break
+        print(run(user_input))
+
+#第十遍，M3，复习AI循环路径
+
+from pi_ai import call_llm
+from tools import TOOLS, run_tool
+
+SYSTEM = "你是一名代码学习助手，主要作用是辅助用户学习代码" + str([t["function"]["name"] for t in TOOLS])
+
+messages = [{"role": "system","content": SYSTEM}]
+
+def run(user_input):
+    messages.append({"role": "user", "content": user_input})
+    while True:
+        msg = call_llm(messages, TOOLS)
+        messages.append(msg)
+        tool_calls = msg.get("tool_calls")
+        if tool_calls:
+            for tc in tool_calls:
+                name = tc["function"]["name"]
+                args_json = tc["function"]["arguments"]
+                out = run_tool(name, args_json)
+                messages.append({
+                    "role": "tool",
+                    "tool_call_id": tc["id"],
+                    "content": out
+                })
+            continue
+        return msg.get("content")
+
+if __name__ == "__main__":
+    while True:
+        user_input = print("你：")
         if user_input == "/bye":
             break
         print(run(user_input))
